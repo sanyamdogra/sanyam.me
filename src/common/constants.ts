05c4db0d2657d188a04d8e3dc2d880b143140544
Sanyam Dogra <sdlord07@gmail.com>
2022-02-01 18:31:16 +0530

    :bug: Bugfix - Fixed the document head missing banner

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,5 @@
 export const SITE_URL: string = "https://www.sanyam.me";
+export const BANNER_URL: string = `${SITE_URL}/static/images/avatar.webp`;
 export const DEFAULT_SITE_DESCRIPTION: string =
   "Personal portfolio/blog of Sanyam Dogra";
 export const DATE_FORMAT: string = "MMM d, yyyy";
--- src/components/shared/documentHead/index.tsx
+++ src/components/shared/documentHead/index.tsx
@@ -1,40 +1,48 @@
 import Head from "next/head";
 
 import React from "react";
-import { DEFAULT_SITE_DESCRIPTION, SITE_URL } from "../../../common/constants";
+import {
+  DEFAULT_SITE_DESCRIPTION,
+  SITE_URL,
+  BANNER_URL
+} from "../../../common/constants";
 
 interface Props {
   pageTitle: string;
   description?: string;
   noIndex?: boolean;
+  imageUrl?: string;
 }
 
 export const DocumentHead: React.FC<Props> = ({
   pageTitle,
   description = DEFAULT_SITE_DESCRIPTION,
   noIndex,
+  imageUrl = BANNER_URL
 }) => {
   const _pageTitle = `${pageTitle} | Sanyam Dogra`;
   return (
     <Head>
       <title>{_pageTitle}</title>
-      <meta name="description" content={description} />
+      <meta name='description' content={description} />
       {noIndex ? (
-        <meta name="robots" content="noindex" />
+        <meta name='robots' content='noindex' />
       ) : (
-        <meta name="robots" content="follow, index" />
+        <meta name='robots' content='follow, index' />
       )}
-      <meta property="og:title" content={pageTitle} />
-      <meta name="og:description" content={description} />
-      <meta property="og:url" content={SITE_URL} />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="Sanyam Dogra" />
+      <meta property='og:title' content={_pageTitle} />
+      <meta name='og:description' content={description} />
+      <meta name='og:image' content={imageUrl} />
+      <meta name='og:image:alt' content={_pageTitle} />
+      <meta property='og:url' content={SITE_URL} />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Sanyam Dogra' />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@sdlord07" />
-      <meta name="twitter:title" content={pageTitle} />
-      <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content={imageURL} /> */}
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@sdlord07' />
+      <meta name='twitter:title' content={_pageTitle} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:image' content={imageUrl} />
     </Head>
   );
 };
